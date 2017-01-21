8869b9cd0fb1763393f50e8ac21cd59b5c75422f
Panayioti Koukoulas <koukoula@cs.ualberta.ca>
2017-01-20 17:01:59 -0700

    All tests passed

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 #  coding: utf-8 
-import SocketServer
+import SocketServer, os, mimetypes
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -28,11 +28,54 @@ import SocketServer
 
 
 class MyWebServer(SocketServer.BaseRequestHandler):
-    
+    directory = os.path.abspath('./www')
+    index = 'index.html'
     def handle(self):
         self.data = self.request.recv(1024).strip()
+        self.dataArray = self.data.split()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall("OK")
+
+        #2 cases: Either get request or not.
+        #If get, actually do something. Else, handle it.
+    	if self.dataArray[0] == 'GET':	
+            self.handleGet()
+        else:   
+            #Return a status code of “405 Method Not Allowed” for any method you cannot handle (POST/PUT/DELETE)     
+            self.request.sendall("HTTP/1.1 405 Method Not Allowed\r\n" + '\r\n' + "405 Method Not Allowed\n")
+
+    def handleGet(self):
+
+        #os.getrealpath 
+        #os.cwd is our current directory
+        #add www to that to get where your serving to that
+        #store that in a seperate variable
+        #ad
+   
+
+        status = "HTTP/1.1 200 OK\r\n"
+        fileText = ''
+        if self.dataArray[1][-1] == '/':
+            path = self.directory + self.dataArray[1] + self.index
+            #path += "Content-Type: text/html\r\n" + '\r\n'
+            #self.request.sendall(path + open("./www/index.html").read())
+
+        else:
+            #path += "Content-Type: text/css\r\n" + '\r\n'
+            #self.request.sendall(path +open("./www/base.css").read())
+            path = self.directory + self.dataArray[1]
+            
+        path = os.path.realpath(path)
+       
+        if os.path.exists(path) and path.startswith(self.directory):
+            mime = mimetypes.guess_type(path)         
+            contentType = "Content-Type: " + mime[0] + '\r\n'
+            
+            fileText = open(path).read()
+
+            self.request.sendall(status + contentType+ '\r\n' + fileText)
+        else:
+            self.request.sendall("HTTP/1.1 404 Not Found\r\n" + '\r\n' + "404 Not Found\n" )
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
