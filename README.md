9d07300720189f994e75cc54eb660160eb4a0522
Noah Shillington <nshillin@ualberta.ca>
2017-01-20 17:40:38 -0700

    Cleaned up code

--- README.md
+++ README.md
@@ -17,8 +17,17 @@ server.py contains contributions from:
 * Abram Hindle
 * Eddie Antonio Santos
 
+* Panayioti Koukoulas
+* Noah Shillington
+
+We consulted with Braedy Kuzma in order to pass the test_get_group (comparing the realpath with the current directory).
+
 But the server.py example is derived from the python documentation
 examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
 
+
+http://docs.python.org/2/library/socketserver.html
+https://docs.python.org/2/library/mimetypes.html
+https://docs.python.org/2/library/os.html
--- server.py
+++ server.py
@@ -1,6 +1,28 @@
 #  coding: utf-8
 import SocketServer, os, mimetypes
 
+# Copyright 2017 Panayioti Koukoulas, Noah Shillington
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Furthermore it is derived from the Python documentation examples thus
+# some of the code is Copyright © 2001-2017 Python Software
+# Foundation; All Rights Reserved
+#
+# http://docs.python.org/2/library/socketserver.html
+# https://docs.python.org/2/library/mimetypes.html
+# https://docs.python.org/2/library/os.html
+
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,7 +39,7 @@ import SocketServer, os, mimetypes
 #
 #
 # Furthermore it is derived from the Python documentation examples thus
-# some of the code is Copyright © 2001-2013 Python Software
+# some of the code is Copyright © 2001-2017 Python Software
 # Foundation; All Rights Reserved
 #
 # http://docs.python.org/2/library/socketserver.html
@@ -30,20 +52,27 @@ import SocketServer, os, mimetypes
 class MyWebServer(SocketServer.BaseRequestHandler):
     directory = os.path.abspath('./www')
     index = 'index.html'
+
+    status = "HTTP/1.1 200 OK\r\n"
+    fileText = ''
+    contentType = ''
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
         self.dataArray = self.data.split()
         print ("Got a request of: %s\n" % self.data)
-        
+
+        # Either 'GET' or 405
     	if self.dataArray[0] == 'GET':
             self.handleGet()
         else:
-            #Return a status code of “405 Method Not Allowed” for any method you cannot handle (POST/PUT/DELETE)
-            self.request.sendall("HTTP/1.1 405 Method Not Allowed\r\n" + '\r\n' + "405 Method Not Allowed\n")
+            # Return a status code of “405 Method Not Allowed” for any method you cannot handle (POST/PUT/DELETE)
+            self.status = "HTTP/1.1 405 Method Not Allowed\r\n"
+            self.fileText = "405 Method Not Allowed\n"
+        self.request.sendall(self.status + self.contentType+ '\r\n' + self.fileText)
 
     def handleGet(self):
-        status = "HTTP/1.1 200 OK\r\n"
-        fileText = ''
+
         path = self.directory + self.dataArray[1]
         if self.dataArray[1][-1] == '/':
             path += self.index
@@ -52,11 +81,12 @@ class MyWebServer(SocketServer.BaseRequestHandler):
 
         if os.path.exists(path) and path.startswith(self.directory):
             mime = mimetypes.guess_type(path)
-            contentType = "Content-Type: " + mime[0] + '\r\n'
-            fileText = open(path).read()
-            self.request.sendall(status + contentType+ '\r\n' + fileText)
+            self.contentType = "Content-Type: " + mime[0] + '\r\n'
+            self.fileText = open(path).read()
+
         else:
-            self.request.sendall("HTTP/1.1 404 Not Found\r\n" + '\r\n' + "404 Not Found\n" )
+            self.status = "HTTP/1.1 404 Not Found\r\n"
+            self.fileText = "404 Not Found\n"
 
 
 if __name__ == "__main__":
